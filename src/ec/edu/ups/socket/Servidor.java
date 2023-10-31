f58ca242fac6fb59df37dedff28dfaa6c06e6ef9
ingrafa <rafikin2013@hotmail.com>
2023-10-30 22:29:18 -0500

    Actualizado

--- src/ec/edu/ups/socket/Servidor.java
+++ src/ec/edu/ups/socket/Servidor.java
@@ -4,17 +4,25 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-
+/* 
+ * Este código implementa un servidor de chat que escucha en un puerto
+ *  específico y permite que varios clientes se conecten.
+ *   Cada cliente que se conecta se maneja en un hilo separado (ClientHandler)
+ *    para que el servidor pueda atender a múltiples clientes simultáneamente.
+ *     Los mensajes enviados por cualquier cliente se retransmiten a todos los clientes
+ *      conectados.
+ * 
+ * */
 public class Servidor {
-    private static final int PORT = 8080;
-    private static Set<PrintWriter> clientWriters = new HashSet<>();
+    private static final int PORT = 8080; // Puerto en el que el servidor escucha
+    private static Set<PrintWriter> clientWriters = new HashSet<>(); // Conjunto de escritores para enviar mensajes a los clientes
 
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
-            System.out.println("Servidor Inicializado en el puerto " + PORT);
+            System.out.println("Servidor Inicializado en el puerto " + PORT); // Muestra un mensaje indicando que el servidor se ha iniciado en un puerto específico
 
             while (true) {
-                new ClientHandler(serverSocket.accept()).start();
+                new ClientHandler(serverSocket.accept()).start(); // Acepta nuevas conexiones de clientes y crea un nuevo hilo para manejar cada cliente
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -23,9 +31,9 @@ public class Servidor {
 
     public static void broadcastMessage(String message) {
         for (PrintWriter writer : clientWriters) {
-            writer.println(message);
+            writer.println(message); // Envía un mensaje a todos los clientes conectados
         }
-        
+
         System.out.println(message); // Imprime el mensaje en la consola del servidor
     }
 
@@ -35,27 +43,27 @@ public class Servidor {
 
         public ClientHandler(Socket socket) {
             this.clientSocket = socket;
-        }	
+        }
 
         public void run() {
             try {
-                out = new PrintWriter(clientSocket.getOutputStream(), true);
-                clientWriters.add(out);
+                out = new PrintWriter(clientSocket.getOutputStream(), true); // Crea un escritor para enviar mensajes al cliente
+                clientWriters.add(out); // Agrega el escritor al conjunto de escritores para enviar mensajes a todos los clientes
 
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); // Crea un lector para recibir mensajes del cliente
                 String message;
 
                 while ((message = in.readLine()) != null) {
-                    Servidor.broadcastMessage(message);
+                    Servidor.broadcastMessage(message); // Envía el mensaje recibido a todos los clientes
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
                 if (out != null) {
-                    clientWriters.remove(out);
+                    clientWriters.remove(out); // Elimina el escritor del conjunto al desconectarse el cliente
                 }
                 try {
-                    clientSocket.close();
+                    clientSocket.close(); // Cierra la conexión con el cliente
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -63,4 +71,3 @@ public class Servidor {
         }
     }
 }
-
